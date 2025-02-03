3201cbcc6f09d176b00c37e25eda92ff09a618ec
Andrey <vovanco03@mail.ru>
2025-02-03 11:31:22 +0300

    used clang-format to update the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello World from " << name << std::endl;
-    return 0;
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello World from " << name << std::endl;
+  return 0;
 }
